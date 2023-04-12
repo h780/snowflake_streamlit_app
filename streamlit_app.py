14eb6b0901694b932d2339252be8e854e03623da
Harsh Sharma <32234187+h780@users.noreply.github.com>
2023-04-11 21:44:16 -0400

    Updated streamlit_app.py
    Changed the structuring of the application

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,7 @@
 import streamlit
 import pandas
 
-my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-streamlit.dataframe(my_fruit_list)
+
 
 streamlit.title('EarthBar')
 streamlit.header('Breakfast Favorites')
@@ -12,3 +11,5 @@ streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avocado Toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
