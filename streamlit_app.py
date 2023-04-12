d26a4bda4f7270f25f56a032840a23bd715e12de
Harsh Sharma <32234187+h780@users.noreply.github.com>
2023-04-11 21:35:13 -0400

    Updated streamlit_app.py
    Added Header and Text

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,7 @@
 import streamlit
 
 streamlit.title('EarthBar')
+streamlit.header('नाश्ता (Breakfast) Menu')
+streamlit.text('Omega 3 and Blueberry OatMeal (ओमेगा 3 और ब्लूबेरी दलिया)')
+streamlit.text('Kale, Spinach & Rocket Smoothie (स्मूदी)')
+streamlit.text('Hard-Boiled Free-Range Egg (उबले हुए अंडे)')
